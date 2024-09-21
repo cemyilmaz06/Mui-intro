0c471d7ee95170d7919ce6622709397e6a5e8157
cem <cemyilmaztech@gmail.com>
2024-09-21 13:02:47 +0300

    MuiInput component

---
+++ src/component/MuiInputElem.jsx
@@ -0,0 +1,132 @@
+import { Box, Container, IconButton } from "@mui/material";
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+const currencies = [
+    {
+      value: 'USD',
+      label: '$',
+    },
+    {
+      value: 'EUR',
+      label: '€',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'JPY',
+      label: '¥',
+    },
+  ];
+
+const MuiInputElem = () => {
+    const [showPassword, setShowPassword] = React.useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    
+  return (
+    <Container>
+      <Box width={500} display="flex" justifyContent="center" flexDirection={"column"}>
+        <TextField type="email" id="outlined-basic" margin="dense" label="Outlined" variant="outlined" fullWidth/>
+        <TextField id="filled-basic" margin="dense" label="Filled" variant="filled" fullWidth/>
+        <TextField id="standard-basic" margin="dense" label="Standard" variant="standard" fullWidth/>
+      </Box>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="input-with-icon-adornment">
+          With a start adornment
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <TextField
+        id="input-with-icon-textfield"
+        label="TextField"
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          },
+        }}
+        // inputProps={{ deprecated
+        //     startAdornment: (
+        //         <InputAdornment position="start">
+        //           <AccountCircle />
+        //         </InputAdornment>
+        //       ),
+        // }}
+        variant="standard"
+      />
+       <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Box component="form" >
+        <div>
+        <TextField
+          id="outlined-select-currency"
+          select
+          label="Select"
+          defaultValue="EUR"
+          helperText="Please select your currency"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          id="outlined-select-currency-native"
+          select
+          label="Native select"
+          defaultValue="EUR"
+          slotProps={{
+            select: {
+              native: true,
+            },
+          }}
+          helperText="Please select your currency"
+        >
+          {currencies.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </TextField>
+      </div>
+        </Box>
+    </Container>
+  );
+};
+
+export default MuiInputElem;
